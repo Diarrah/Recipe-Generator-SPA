374e80e03ea5dfe215ed48dcbceae63ce4d64976
Diarrah <diarrahcasiano@gmail.com>
2020-11-08 19:47:46 -0800

    Context provider around App component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import RecipeContextProvider from './recipeContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <RecipeContextProvider>
+        <App />
+      </RecipeContextProvider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
