e78d5c8ab246c042c2ccb67a21245d61f2e2eacb
Diarrah <diarrahcasiano@gmail.com>
2020-11-08 21:22:17 -0800

    Title added to iframe for accessibility

--- src/slides/Video.js
+++ src/slides/Video.js
@@ -2,11 +2,12 @@ import React, { useContext } from 'react';
 import { RecipeContext } from '../recipeContext';
 
 const Video = () => {
-    const { meal: {videoLink} } = useContext(RecipeContext);
+    const { meal: {meal, videoLink} } = useContext(RecipeContext);
 
     return (
         <div className="video__wrapper">
             <iframe 
+                title={`Video showing how to cook ${meal}`}
                 className="video"
                 width="420" height="315"
                 src={videoLink && `https://www.youtube.com/embed/${videoLink.slice(-11)}`}>
