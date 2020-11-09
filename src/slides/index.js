014da95923ff633bd5f4f9b4b897fba2eee57347
Diarrah <diarrahcasiano@gmail.com>
2020-11-08 19:48:37 -0800

    Grouping for easy import

---
+++ src/slides/index.js
@@ -0,0 +1,6 @@
+export { default as Welcome } from './Welcome';
+export { default as Meal } from './Meal';
+export { default as Ingredients } from './Ingredients';
+export { default as Steps } from './Steps';
+export { default as Video } from './Video';
+export { default as ThankYou } from './ThankYou';
