4df2ca50f289b44f39d4fd5cfbf0955472d7d51b
Diarrah <diarrahcasiano@gmail.com>
2020-11-08 19:55:32 -0800

    YT embeded video

---
+++ src/slides/Video.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { RecipeContext } from '../recipeContext';
+
+const Video = () => {
+    const { meal: {videoLink} } = useContext(RecipeContext);
+
+    return (
+        <div className="video__wrapper">
+            <iframe 
+                className="video"
+                width="420" height="315"
+                src={videoLink && `http://www.youtube.com/embed/${videoLink.slice(-11)}`}>
+			</iframe>
+        </div>
+    )
+}
+
+export default Video
